96ced243450564d5b26572de2732b45edd903856
Cédric Menzi <cedric.menzi@buhlergroup.com>
2022-06-28 16:49:32 +0200

    repo added

---
+++ source/src/Car/Persistence/CarRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace CarRent.Car.Persistence
+{
+    using System;
+
+    using CarRent.Car.Domain;
+
+    public class CarRepository : ICarRepository
+    {
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetByCarNumber(string carNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
